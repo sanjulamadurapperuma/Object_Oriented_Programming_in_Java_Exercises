bc51b6ba145dc0b41c20550c5ed3a1f628c5eb61
Sanjula Madurapperuma <sanjula99@gmail.com>
2018-10-01 15:21:13 +0530

    Committing Director and Test classes Tutorial 2

---
+++ src/Week_2/Director.java
@@ -0,0 +1,39 @@
+package Week_2;
+
+import java.util.Date;
+
+public class Director {
+    private String name;
+    private String surname;
+    private int numDirectedMovie;
+    private Date dob;
+
+    public Director(String name, String surname) {
+        this.name = name;
+        this.surname = surname;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public int getNumDirectedMovie() {
+        return numDirectedMovie;
+    }
+
+    public void setNumDirectedMovie(int numDirectedMovie) {
+        this.numDirectedMovie = numDirectedMovie;
+    }
+
+    public Date getDob() {
+        return dob;
+    }
+
+    public void setDob(Date dob) {
+        this.dob = dob;
+    }
+}
