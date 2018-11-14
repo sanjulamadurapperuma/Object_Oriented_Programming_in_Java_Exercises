093a6d231fc6c0893dd6b071b8d8e5997c979035
Sanjula Madurapperuma <sanjula99@gmail.com>
2018-11-14 08:11:20 +0530

    Updated TestBook.java with implementation of compareTo() method

--- src/com/sanjula/oop/java/tutorials/week_5/TestBook.java
+++ src/com/sanjula/oop/java/tutorials/week_5/TestBook.java
@@ -9,6 +9,7 @@ public class TestBook {
         int selectedOption = 1;
         Scanner sc = new Scanner(System.in);
 
+        //Do-while loop to implement menu - Enter 0 to exit
         do{
             System.out.println("Please enter the details of the book as follows: ");
             System.out.println();
@@ -35,40 +36,51 @@ public class TestBook {
                 book.toString();
             }
 
+            /*This is how to declare a HashMap*/
+            HashMap<Book, Integer> hmap = new HashMap<>();
+            //Insert the key for each book
+            for (int i = 0; i < bookList.size(); i++){
+                System.out.println("Please enter the number of the shelf where the book is placed: "
+                        + bookList.get(i).getTitle());
+
+                int numShelf = sc.nextInt();
+
+                /*Adding elements to the hashmap*/
+                hmap.put(bookList.get(i), numShelf);
+            }
+
+
+            System.out.println("Insert the number of the shelf");
+            int selectedShelf = sc.nextInt();
+            System.out.println("The book in shelf " + selectedShelf + " are: ");
+
+            /*Display content using iterator*/
+
+            Set set = hmap.entrySet();
+            Iterator iterator = set.iterator();
+            while (iterator.hasNext()){
+                Map.Entry entry = (Map.Entry)iterator.next();
+
+                if (selectedShelf == (int) entry.getValue()){
+                    System.out.println(((Book)entry.getKey()).getTitle());
+                }
+            }
+
+            //Implementation of the compareTo method
+            Book book1 = new Book("Steve Jobs", 2011, "Walter Isaacson");
+            book1.setPrice(34.20);
+
+            Book book2 = new Book("Charlie and the Chocolate Factory", 1989, "Roald Dahl");
+            book2.setPrice(20.10);
+
+            int yearDifference = book1.compareTo(book2);
+            System.out.println();
+            System.out.println("What is the difference between the publish years? " + yearDifference);
+
+
             System.out.println();
             System.out.println("Press '0' if you want to exit the program");
             selectedOption = sc.nextInt();
         }while(selectedOption!=0);
-
-        /*This is how to declare a HashMap*/
-        HashMap<Book, Integer> hmap = new HashMap<>();
-        //Insert the key for each book
-        for (int i = 0; i < bookList.size(); i++){
-            System.out.println("Please enter the number of the shelf where the book is placed: "
-                    + bookList.get(i).getTitle());
-
-            int numShelf = sc.nextInt();
-
-            /*Adding elements to the hashmap*/
-            hmap.put(bookList.get(i), numShelf);
-        }
-
-
-        System.out.println("Insert the number of the shelf");
-        int selectedShelf = sc.nextInt();
-        System.out.println("The book in shelf " + selectedShelf + " are: ");
-
-        /*Display content using iterator*/
-
-        Set set = hmap.entrySet();
-        Iterator iterator = set.iterator();
-        while (iterator.hasNext()){
-            Map.Entry entry = (Map.Entry)iterator.next();
-
-            if (selectedShelf == (int) entry.getValue()){
-                System.out.println(((Book)entry.getKey()).getTitle());
-            }
-        }
-
     }
 }
