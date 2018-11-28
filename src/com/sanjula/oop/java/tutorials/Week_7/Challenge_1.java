db5a3e1a26cceb0bf7b03c224f131c330d7a2dcd
Sanjula Madurapperuma <sanjula99@gmail.com>
2018-11-28 06:14:51 +0530

    Completed Challenge Question 1

---
+++ src/com/sanjula/oop/java/tutorials/Week_7/Challenge_1.java
@@ -0,0 +1,31 @@
+package com.sanjula.oop.java.tutorials.Week_7;
+
+public class Challenge_1 {
+    private boolean uniqueChars(String str){
+        //Brute-force technique used
+        //If 2 of the same characters are encountered,
+        // then return false
+        for (int i = 0; i < str.length(); i++){
+            for (int j = 1; j < str.length(); j++){
+                if (str.charAt(i) == str.charAt(j)){
+                    return false;
+                }
+            }
+        }
+        //If no duplicate characters are found,
+        // then return true
+        return true;
+    }
+
+    public static void main(String[] args) {
+        Challenge_1 ch1 = new Challenge_1();
+        String str1 = "Excellence";
+
+        if (ch1.uniqueChars(str1)){
+            System.out.println("The string " + str1 + " has all unique characters");
+        } else{
+            System.out.println("The string " + str1 + " has duplicate characters");
+        }
+
+    }
+}
